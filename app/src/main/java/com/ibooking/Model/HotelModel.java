f2fbf9222d9d09223609f6fafa349eb4d5985667
Anh Hoang <anh.t.hoang@sjsu.edu>
2021-04-25 03:38:04 -0700

    add HotelModel

--- app/src/main/java/com/ibooking/Model/HotelModel.java
+++ app/src/main/java/com/ibooking/Model/HotelModel.java
@@ -1,4 +1,34 @@
 package com.ibooking.Model;
 
 public class HotelModel {
+    private String address;
+    private String zip;
+    private double rating;
+
+    public HotelModel( String address, String zip, double rating){
+        this.address = address;
+        this.zip = zip;
+        this.rating= rating;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    @Override
+    public String toString() {
+        return "HotelModel{" +
+                "address='" + address + '\'' +
+                ", zip='" + zip + '\'' +
+                ", rating=" + rating +
+                '}';
+    }
 }
