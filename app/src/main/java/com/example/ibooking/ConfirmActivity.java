b0a903727030e20ff830a1d8f850a99e6d2768b5
Nhien Lam <nhienlam.ricky@gmail.com>
2021-05-11 15:39:49 -0700

    update ConfirmActivity

---
+++ app/src/main/java/com/example/ibooking/ConfirmActivity.java
@@ -0,0 +1,46 @@
+package com.example.ibooking;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.ibooking.Common.Common;
+
+public class ConfirmActivity extends AppCompatActivity {
+
+    private Button confirmButton;
+    private TextView txt_hotel_address, txt_booking_checkin_date_text, txt_booking_checkout_date_text, txt_room_type_text;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_confirm);
+
+        confirmButton = findViewById(R.id.confirm_button);
+
+        txt_hotel_address = findViewById(R.id.txt_hotel_address);
+        txt_booking_checkin_date_text = findViewById(R.id.txt_booking_checkin_date_text);
+        txt_booking_checkout_date_text = findViewById(R.id.txt_booking_checkout_date_text);
+        txt_room_type_text = findViewById(R.id.txt_room_type_text);
+
+        confirmButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(ConfirmActivity.this , HomeActivity.class));
+            }
+        });
+        setReservationInfo();
+    }
+
+    private void setReservationInfo()
+    {
+        txt_hotel_address.setText(Common.currentHotel.getAddress());
+        txt_booking_checkin_date_text.setText(Common.currentCheckin);
+        txt_booking_checkout_date_text.setText(Common.currentCheckout);
+        txt_room_type_text.setText(Common.currentRoom.getRoomType());
+    }
+}
