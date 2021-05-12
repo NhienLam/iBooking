8300ad999bdc438daa64e5a2b3f0073ec19eeb30
Nhien Lam <nhienlam.ricky@gmail.com>
2021-05-12 14:31:32 -0700

    Display message after successfully making a reservation

--- app/src/main/java/com/example/ibooking/ConfirmActivity.java
+++ app/src/main/java/com/example/ibooking/ConfirmActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.ibooking.Common.Common;
 import com.example.ibooking.Model.ReservationModel;
@@ -38,6 +39,7 @@ public class ConfirmActivity extends AppCompatActivity {
             public void onClick(View v) {
                 // Unable to convert String mmmm-d-yyyy to localdate
                 // Common.currentReservation = new ReservationModel( -1, Common.currentHotel.getHotelID(), Common.currentCheckin, Common.currentCheckout)
+                Toast.makeText(ConfirmActivity.this, "Successfully made a reservation!", Toast.LENGTH_SHORT).show();
                 startActivity(new Intent(ConfirmActivity.this , HomeActivity.class));
             }
         });
@@ -47,8 +49,8 @@ public class ConfirmActivity extends AppCompatActivity {
     private void setReservationInfo()
     {
         txt_hotel_address.setText(Common.currentHotel.getAddress());
-        txt_booking_checkin_date_text.setText(Common.currentCheckin);
-        txt_booking_checkout_date_text.setText(Common.currentCheckout);
+        txt_booking_checkin_date_text.setText("Check In: " + Common.currentCheckin);
+        txt_booking_checkout_date_text.setText("Check Out: " + Common.currentCheckout);
         txt_room_type_text.setText("Room: " + Common.currentRoom.getRoomType());
         txt_price_text.setText(String.valueOf(Common.currentRoom.getPrice()) + "/day");
     }
