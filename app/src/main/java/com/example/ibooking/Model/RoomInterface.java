d3614da8227872c4648d903fcf071c0bef681da6
Nhien (Ricky) Lam <62775270+NhienLam@users.noreply.github.com>
2021-05-19 10:00:31 -0700

    Update RoomInterface.java

--- app/src/main/java/com/example/ibooking/Model/RoomInterface.java
+++ app/src/main/java/com/example/ibooking/Model/RoomInterface.java
@@ -1,11 +1,49 @@
 package com.example.ibooking.Model;
 
+/**
+ * A simple Hotel Room
+ */
 public interface RoomInterface {
+    /**
+     * Gets id of room
+     * @return id of room
+     */
     int getRoomId();
+    
+    /**
+     * Gets hotel id
+     * @return hotel id
+     */
     int getHotelId();
+    
+    /**
+     * Gets room type
+     * @return room type
+     */
     String getRoomType();
+    
+    /**
+     * Gets capacity of room
+     * @return capacity of room
+     */
     int getCapacity();
+    
+    /**
+     * Gets price of room
+     * @return price of room
+     */
     double getPrice();
+    
+    /**
+     * Gets availability status of the room
+     * @return availability status of the room
+     */
     boolean isAvailable();
+    
+    /**
+     * Add an extra service to the room reservation using Decorator Design Pattern
+     * @param room the target room
+     * @param newService the extra service to add
+     */
     RoomInterface addService(RoomInterface room, String newService);
 }
